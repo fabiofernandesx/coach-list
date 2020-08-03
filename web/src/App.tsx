2cd1a2e56ddabdfc588d45f33137768156e547a5
Fabio Fernandes <fabio@fabiofernandes.net>
2020-08-03 12:11:16 -0500

    barebones

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <h1>The Coach List</h1>
+      <h2>https://coach-list.xyz</h2>
+    </div>
+  );
+}
+
+export default App;
