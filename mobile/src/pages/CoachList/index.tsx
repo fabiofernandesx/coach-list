81a954f5d4731dbe63cbbea7165b0520eaa0191f
Fabio Fernandes <fabio@fabiofernandes.net>
2020-08-09 16:09:21 -0400

    add filter

--- mobile/src/pages/CoachList/index.tsx
+++ mobile/src/pages/CoachList/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text } from 'react-native'
 import styles from './styles'
 import PageHeader from '../../components/PageHeader'
 import CoachItem from '../../components/CoachItem'
-import { ScrollView } from 'react-native-gesture-handler'
+import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
 
 const CoachList = () => {
+  const [isFilterVisible, setFilterVisibility] = useState(false);
+  function handleToggleFiltersVisibility() {
+    setFilterVisibility(!isFilterVisible)
+  }
   return (
     <View style={styles.container}>
-      <PageHeader title="Coach List" />
+      <PageHeader title="Coach List" headerRight={(
+        <BorderlessButton onPress={handleToggleFiltersVisibility}>
+          <Feather name="filter" size={20} color="#FFF" />
+        </BorderlessButton>
+      )}>
+        {isFilterVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Area</Text>
+            <TextInput style={styles.input} />
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Weekday</Text>
+                <TextInput style={styles.input} />
+              </View>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Time</Text>
+                <TextInput style={styles.input} />
+              </View>
+            </View>
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filter</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
       <ScrollView style={styles.scroll} contentContainerStyle={{
         paddingHorizontal: 16,
         paddingBottom: 16
--- mobile/src/pages/CoachList/styles.ts
+++ mobile/src/pages/CoachList/styles.ts
@@ -7,6 +7,42 @@ const styles = StyleSheet.create({
   },
   scroll: {
     marginTop: -40,
+  },
+  searchForm: {
+    marginBottom: 24
+  },
+  label: {
+    color: '#d4c2ff',
+    fontFamily: 'Poppins_400Regular'
+  },
+  input: {
+    height: 54,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16
+  },
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  inputBlock: {
+    width: '48%'
+  },
+  submitButton: {
+    backgroundColor: '#04d361',
+    height: 56,
+    flexDirection: 'row',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submitButtonText: {
+    color: '#FFF',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16
   }
 })
 export default styles
